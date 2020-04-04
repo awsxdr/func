36237d2713e316a43cc3b9e2a8a3c1f889305154
Daniel Errington <derrington@gmail.com>
2020-04-04 12:06:44 +0100

    Add option functionality

---
+++ FunCSharp.UnitTests/OptionTests.cs
@@ -0,0 +1,78 @@
+﻿namespace FunCSharp.UnitTests
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+    using Option;
+
+    using static Option.Option;
+
+    [TestFixture]
+    public class OptionTests
+    {
+        [Test]
+        public void Option_WithValue_IsSome()
+        {
+            var option = Some(1234);
+
+            (option is Some).Should().BeTrue();
+        }
+
+        [Test]
+        public void Option_WithValue_IsTypedSome()
+        {
+            var option = Some(1234);
+
+            (option is Some<int>).Should().BeTrue();
+        }
+
+        [Test]
+        public void Option_WithValue_WorksWithSwitchStatement()
+        {
+            var option = Some(1234);
+
+            var result = option switch
+            {
+                Some<string> _ => false,
+                Some<int> _ => true,
+                Some _ => false,
+                None _ => false,
+                _ => false
+            };
+
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void Option_WithoutValue_IsNone()
+        {
+            var option = None();
+
+            (option is None).Should().BeTrue();
+        }
+
+        [Test]
+        public void Option_WithoutValue_WorksWithSwitchStatement()
+        {
+            var option = None();
+
+            var result = option switch
+            {
+                Some<string> _ => false,
+                Some<int> _ => false,
+                Some _ => false,
+                None _ => true,
+                _ => false
+            };
+
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void Option_WithValue_CanRetrieveValue()
+        {
+            var option = Some(1234);
+
+            (option as Some<int>).Value.Should().Be(1234);
+        }
+    }
+}
---
+++ FunCSharp/Option/Option.cs
@@ -0,0 +1,34 @@
+﻿namespace FunCSharp.Option
+{
+    public interface Option
+    {
+        public static Option Some<TValue>(TValue value) =>
+            new SomeClass<TValue>(value);
+
+        public static Option None() => new NoneClass();
+    }
+
+    public interface None : Option
+    {
+    }
+
+    public interface Some : Option
+    {
+    }
+
+    public interface Some<TValue> : Some
+    {
+        TValue Value { get; }
+    }
+
+    internal class NoneClass : None
+    {
+    }
+
+    internal class SomeClass<TValue> : Some<TValue>
+    {
+        public TValue Value { get; }
+
+        internal SomeClass(TValue value) => Value = value;
+    }
+}
