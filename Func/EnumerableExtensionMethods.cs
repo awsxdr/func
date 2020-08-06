f5310a9413e00bf0cc08c2a8e3711e903615ff8e
Daniel Errington <daniel.errington@blueprism.com>
2020-08-06 11:56:21 +0100

    Add ForEach and Evaluate enumerable methods

---
+++ Func/EnumerableExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿namespace Func
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class EnumerableExtensionMethods
+    {
+        public static IEnumerable<T> ForEach<T>(this IEnumerable<T> @this, Action<T> func) =>
+            @this.Select(x => x.Tee(func));
+
+        public static IEnumerable<T> ForEach<T, TIgnored>(this IEnumerable<T> @this, Func<T, TIgnored> func) =>
+            @this.Select(x => x.Tee(func));
+
+        public static void Evaluate<T>(this IEnumerable<T> @this) =>
+            _ = @this.ToList();
+    }
+}
