5128d7f90db3acc32793bcdd60c4fb08f5a0cd3d
Daniel Errington <daniel.errington@blueprism.com>
2020-10-19 15:42:19 +0100

    Remove partial ThenMap functions

--- Func/Results/ResultMapExtensionMethods.cs
+++ Func/Results/ResultMapExtensionMethods.cs
@@ -46,9 +46,6 @@
 		public static Result<TResult> ThenMap<TValue1, TValue2, TValue3, TResult>(this Result<TValue3> result, Func<TValue1, TValue2, TValue3, TResult> func, TValue1 value1, TValue2 value2) =>
 			result.Then(value3 => func(value1, value2, value3).Map(Succeed));
 
-		public static Func<TValue2, TValue3, Result<TResult>> ThenMap<TValue1, TValue2, TValue3, TResult>(this Result<TValue1> result, Func<TValue1, TValue2, TValue3, TResult> func) =>
-			(value2, value3) => result.Then(value1 => func(value1, value2, value3).Map(Succeed));
-
 		public static Task<Result<TResult>> ThenMap<TValue1, TValue2, TValue3, TValue4, TResult>(this Task<Result<TValue4>> valueTask, Func<TValue1, TValue2, TValue3, TValue4, Task<TResult>> func, TValue1 value1, TValue2 value2, TValue3 value3) =>
 			valueTask.ContinueWith(result => result.Result.Then(value4 => func(value1, value2, value3, value4).Map(Succeed<TResult>))).Unwrap();
 
@@ -60,12 +57,6 @@
 
 		public static Result<TResult> ThenMap<TValue1, TValue2, TValue3, TValue4, TResult>(this Result<TValue4> result, Func<TValue1, TValue2, TValue3, TValue4, TResult> func, TValue1 value1, TValue2 value2, TValue3 value3) =>
 			result.Then(value4 => func(value1, value2, value3, value4).Map(Succeed));
-
-		public static Func<TValue2, TValue3, TValue4, Result<TResult>> ThenMap<TValue1, TValue2, TValue3, TValue4, TResult>(this Result<TValue1> result, Func<TValue1, TValue2, TValue3, TValue4, TResult> func) =>
-			(value2, value3, value4) => result.Then(value1 => func(value1, value2, value3, value4).Map(Succeed));
-
-		public static Func<TValue3, TValue4, Result<TResult>> ThenMap<TValue1, TValue2, TValue3, TValue4, TResult>(this Result<TValue2> result, Func<TValue1, TValue2, TValue3, TValue4, TResult> func, TValue1 value1) =>
-			(value3, value4) => result.Then(value2 => func(value1, value2, value3, value4).Map(Succeed));
 
 		public static Task<Result<TResult>> ThenMap<TValue1, TValue2, TValue3, TValue4, TValue5, TResult>(this Task<Result<TValue5>> valueTask, Func<TValue1, TValue2, TValue3, TValue4, TValue5, Task<TResult>> func, TValue1 value1, TValue2 value2, TValue3 value3, TValue4 value4) =>
 			valueTask.ContinueWith(result => result.Result.Then(value5 => func(value1, value2, value3, value4, value5).Map(Succeed<TResult>))).Unwrap();
@@ -79,15 +70,6 @@
 		public static Result<TResult> ThenMap<TValue1, TValue2, TValue3, TValue4, TValue5, TResult>(this Result<TValue5> result, Func<TValue1, TValue2, TValue3, TValue4, TValue5, TResult> func, TValue1 value1, TValue2 value2, TValue3 value3, TValue4 value4) =>
 			result.Then(value5 => func(value1, value2, value3, value4, value5).Map(Succeed));
 
-		public static Func<TValue2, TValue3, TValue4, TValue5, Result<TResult>> ThenMap<TValue1, TValue2, TValue3, TValue4, TValue5, TResult>(this Result<TValue1> result, Func<TValue1, TValue2, TValue3, TValue4, TValue5, TResult> func) =>
-			(value2, value3, value4, value5) => result.Then(value1 => func(value1, value2, value3, value4, value5).Map(Succeed));
-
-		public static Func<TValue3, TValue4, TValue5, Result<TResult>> ThenMap<TValue1, TValue2, TValue3, TValue4, TValue5, TResult>(this Result<TValue2> result, Func<TValue1, TValue2, TValue3, TValue4, TValue5, TResult> func, TValue1 value1) =>
-			(value3, value4, value5) => result.Then(value2 => func(value1, value2, value3, value4, value5).Map(Succeed));
-
-		public static Func<TValue4, TValue5, Result<TResult>> ThenMap<TValue1, TValue2, TValue3, TValue4, TValue5, TResult>(this Result<TValue3> result, Func<TValue1, TValue2, TValue3, TValue4, TValue5, TResult> func, TValue1 value1, TValue2 value2) =>
-			(value4, value5) => result.Then(value3 => func(value1, value2, value3, value4, value5).Map(Succeed));
-
 		public static Task<Result<TResult>> ThenMap<TValue1, TValue2, TValue3, TValue4, TValue5, TValue6, TResult>(this Task<Result<TValue6>> valueTask, Func<TValue1, TValue2, TValue3, TValue4, TValue5, TValue6, Task<TResult>> func, TValue1 value1, TValue2 value2, TValue3 value3, TValue4 value4, TValue5 value5) =>
 			valueTask.ContinueWith(result => result.Result.Then(value6 => func(value1, value2, value3, value4, value5, value6).Map(Succeed<TResult>))).Unwrap();
 
@@ -99,18 +81,6 @@
 
 		public static Result<TResult> ThenMap<TValue1, TValue2, TValue3, TValue4, TValue5, TValue6, TResult>(this Result<TValue6> result, Func<TValue1, TValue2, TValue3, TValue4, TValue5, TValue6, TResult> func, TValue1 value1, TValue2 value2, TValue3 value3, TValue4 value4, TValue5 value5) =>
 			result.Then(value6 => func(value1, value2, value3, value4, value5, value6).Map(Succeed));
-
-		public static Func<TValue2, TValue3, TValue4, TValue5, TValue6, Result<TResult>> ThenMap<TValue1, TValue2, TValue3, TValue4, TValue5, TValue6, TResult>(this Result<TValue1> result, Func<TValue1, TValue2, TValue3, TValue4, TValue5, TValue6, TResult> func) =>
-			(value2, value3, value4, value5, value6) => result.Then(value1 => func(value1, value2, value3, value4, value5, value6).Map(Succeed));
-
-		public static Func<TValue3, TValue4, TValue5, TValue6, Result<TResult>> ThenMap<TValue1, TValue2, TValue3, TValue4, TValue5, TValue6, TResult>(this Result<TValue2> result, Func<TValue1, TValue2, TValue3, TValue4, TValue5, TValue6, TResult> func, TValue1 value1) =>
-			(value3, value4, value5, value6) => result.Then(value2 => func(value1, value2, value3, value4, value5, value6).Map(Succeed));
-
-		public static Func<TValue4, TValue5, TValue6, Result<TResult>> ThenMap<TValue1, TValue2, TValue3, TValue4, TValue5, TValue6, TResult>(this Result<TValue3> result, Func<TValue1, TValue2, TValue3, TValue4, TValue5, TValue6, TResult> func, TValue1 value1, TValue2 value2) =>
-			(value4, value5, value6) => result.Then(value3 => func(value1, value2, value3, value4, value5, value6).Map(Succeed));
-
-		public static Func<TValue5, TValue6, Result<TResult>> ThenMap<TValue1, TValue2, TValue3, TValue4, TValue5, TValue6, TResult>(this Result<TValue4> result, Func<TValue1, TValue2, TValue3, TValue4, TValue5, TValue6, TResult> func, TValue1 value1, TValue2 value2, TValue3 value3) =>
-			(value5, value6) => result.Then(value4 => func(value1, value2, value3, value4, value5, value6).Map(Succeed));
 
 	}
 }
